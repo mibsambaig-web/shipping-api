911c968c8a0b7843536c813505a9a97eff834743
Mibsam Baig <bismillahenterprises@Mibsams-MacBook-Air.local>
2026-07-31 02:13:54 +0500

    program.cs file fixed

--- Program.cs
+++ Program.cs
@@ -10,12 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 // Database
-builder.Services.AddDbContext<AppDbContext>(options =>
-    var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-    var uri = new Uri(databaseUrl!);
-    var userInfo = uri.UserInfo.Split(':');
-    var connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]}";
-    options.UseNpgsql(connectionString);
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+var uri = new Uri(databaseUrl!);
+var userInfo = uri.UserInfo.Split(':');
+var connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]}";
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
 // JWT
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
